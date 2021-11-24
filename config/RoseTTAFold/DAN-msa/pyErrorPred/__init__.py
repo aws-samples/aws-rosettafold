d4789311d422c9d7dff996e1df2a2ce73eb4d47e
Brian Loyal <bloyal@amazon.com>
2021-11-24 16:10:39 -0600

    Remove RoseTTAFold repo due to limits on CC size

--- config/RoseTTAFold/DAN-msa/pyErrorPred/__init__.py
+++
@@ -1,9 +0,0 @@
-from .analyze import *
-from .dataProcessingUtils import *
-from .deepLearningUtils import *
-from .resnet import *
-from .model import *
-from .pyprotein import *
-from .featurize import *
-from .predict import*
-from .layers import*
